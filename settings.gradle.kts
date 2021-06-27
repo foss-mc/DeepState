e3a20e79f67775a199cfb9e569895a2763b42a39
Alexander <protonull@protonmail.com>
2021-06-27 22:49:07 +0100

    Setup project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "DeepState"
+
+include("DeepState-API", "DeepState-Server")
